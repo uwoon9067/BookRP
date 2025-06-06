a5426f17d6c284e8e7a69e52807ce2933af0ac09
firefox9067 <firefox9067@gmail.com>
2025-06-06 23:00:14 +0900

    Initialize project

---
+++ BookRP/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace BookRP
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+
+        }
+    }
+
+}
